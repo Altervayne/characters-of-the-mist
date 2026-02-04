d2a481a437f30de8e091c05d03ba93c98b3e9ee7
Florian Douay <flooooo85@gmail.com>
2026-02-04 10:00:51 +0100

    ignored linter warning for SetFolderName within UseEffect as it is intentional

--- src/components/mobile/MobileAddFolderSheet.tsx
+++ src/components/mobile/MobileAddFolderSheet.tsx
@@ -31,6 +31,7 @@ export default function MobileAddFolderSheet({
 	// Reset folder name when sheet opens
 	useEffect(() => {
 		if (isOpen) {
+			// eslint-disable-next-line react-hooks/set-state-in-effect
 			setFolderName('');
 		}
 	}, [isOpen]);
